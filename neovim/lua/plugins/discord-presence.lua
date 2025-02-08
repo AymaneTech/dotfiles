ab3c9c4063db6020c510b7e3a09fbcb24705040c
Aymane El Maini <elmainiaymane03@gmail.com>
2025-02-08 05:44:51 +0100

    add some plugins to neovim
    added plugins:
      - auto save
      - comments
      - discord presence

---
+++ neovim/lua/plugins/discord-presence.lua
@@ -0,0 +1,11 @@
+return {
+  "vyfor/cord.nvim",
+  build = "./build || .\\build",
+  event = "VeryLazy",
+  run = "./build || .\\build",
+  config = function()
+    require("cord").setup()
+  end,
+  opts = {},
+}
+
